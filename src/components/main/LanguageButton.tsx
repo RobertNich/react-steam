2c13e4e28a0041e4e7b7cb465cfe006ec9609c16
Robert <ro13ertnichols@gmail.com>
2023-08-08 16:04:17 +0930

    added the start of the banner page

--- src/components/main/LanguageButton.tsx
+++ src/components/main/LanguageButton.tsx
@@ -1,7 +1,22 @@
+import downArrow from "../../assets/icon-arrow-down.svg";
+
 export const LanguageButton = () => {
   function changeLanguage() {
     console.log("changing language");
   }
 
-  return <button onClick={changeLanguage}>language</button>;
+  return (
+    <button
+      style={{
+        color: "#b9b9b9",
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+      }}
+      onClick={changeLanguage}
+    >
+      <p>language</p>
+      <img width={10} src={downArrow} alt="arrow-down" />
+    </button>
+  );
 };
---
+++ src/components/store/StoreBanner.tsx
@@ -0,0 +1,82 @@
+import { Link } from "react-router-dom";
+import { Dropdown } from "../common/Dropdown";
+import searchIcon from "../../assets/icon-search.svg";
+
+export const StoreBanner = () => {
+  const yourStoreItems = [
+    {
+      name: "Home",
+      link: "",
+    },
+    {
+      name: "Community Recommendations",
+      link: "",
+    },
+    {
+      name: "Recently Viewed",
+      link: "",
+    },
+    {
+      name: "Steam Curators",
+      link: "",
+    },
+  ];
+
+  const newsItems = [
+    {
+      name: "Steam Replay 2022",
+      link: "",
+    },
+    {
+      name: "Top Sellers",
+      link: "",
+    },
+    {
+      name: "Most Played",
+      link: "",
+    },
+    {
+      name: "New & Trending",
+      link: "",
+    },
+    {
+      name: "Special Offers",
+      link: "",
+    },
+    {
+      name: "Recently Updated",
+      link: "",
+    },
+    {
+      name: "Popular Upcoming",
+      link: "",
+    },
+  ];
+
+  return (
+    <>
+      <div className="banner-image">
+        <div className="banner-bar">
+          <div className="banner-links">
+            <Dropdown label="Your Store" items={yourStoreItems} />
+            <Dropdown label="News & Noteworthy" items={newsItems} />
+            <Link to="">Categories</Link>
+            <Link to="">Points Shop</Link>
+            <Link to="">News</Link>
+            <Link to="">Labs</Link>
+          </div>
+          <div className="banner-search-bar">
+            <p>search</p>
+            <img
+              src={searchIcon}
+              width={20}
+              alt="search"
+              className="search-icon"
+            />
+          </div>
+        </div>
+        <Link className="banner-clickable" to="" />
+      </div>
+    </>
+  );
+};
