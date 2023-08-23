730e673b16fbfeb08e28911931f9b35b46ce23f6
Robert <ro13ertnichols@gmail.com>
2023-08-23 20:33:48 +0930

    tweaked some css

--- src/components/common/InfoCarousel.tsx
+++ src/components/common/InfoCarousel.tsx
@@ -46,63 +46,59 @@ export const InfoCarousel = ({ title, slides }: Props) => {
       >
         &lt;
       </button>
-      <div>
-        <div className="flex-column-start uppercase">
-          {title ? <h4>{title}</h4> : <></>}
-          <div className="carousel-slide">
-            <img
-              src={hoveredImage}
-              alt={`${slides[currentIndex].title}-cover`}
-              className="carousel-image"
-            />
+      <div className="flex-column-start uppercase">
+        {title ? <h4>{title}</h4> : <></>}
+        <div className="carousel-slide">
+          <img
+            src={hoveredImage}
+            alt={`${slides[currentIndex].title}-cover`}
+            className="carousel-image"
+          />
 
-            <div className="slide-info">
-              <h3>{slides[currentIndex].title}</h3>
-              <div className="carousel-thumbnails-container">
-                {slides[currentIndex].thumbnails.map((thumbnail) => (
-                  <img
-                    src={thumbnail}
-                    alt="thumbnail"
-                    className="carousel-thumbnail"
-                    onMouseEnter={() => setHoveredImage(thumbnail)}
-                    onMouseLeave={() =>
-                      setHoveredImage(slides[currentIndex].cover)
-                    }
-                  />
+          <div className="slide-info">
+            <h3>{slides[currentIndex].title}</h3>
+            <div className="carousel-thumbnails-container">
+              {slides[currentIndex].thumbnails.map((thumbnail) => (
+                <img
+                  src={thumbnail}
+                  alt="thumbnail"
+                  className="carousel-thumbnail"
+                  onMouseEnter={() => setHoveredImage(thumbnail)}
+                  onMouseLeave={() =>
+                    setHoveredImage(slides[currentIndex].cover)
+                  }
+                />
+              ))}
+            </div>
+            <h4>{slides[currentIndex].subtitle}</h4>
+            <div className="carousel-price-platform">
+              <p>{slides[currentIndex].price}</p>
+              <div className="carousel-platforms">
+                {slides[currentIndex].platforms.map((platform) => (
+                  <PlatformIcon platform={platform} />
                 ))}
-              </div>
-              <h4>{slides[currentIndex].subtitle}</h4>
-              <div className="carousel-price-platform">
-                <p>{slides[currentIndex].price}</p>
-                <div className="carousel-platforms">
-                  {slides[currentIndex].platforms.map((platform) => (
-                    <PlatformIcon platform={platform} />
-                  ))}
-                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="carousel-indicators">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => goToImage(index)}
+              className={`carousel-indicator ${
+                index === currentIndex ? "active" : ""
+              }`}
+            />
+          ))}
+        </div>
       </div>
-
       <button
         onClick={nextImage}
         className="carousel-side-buttons carousel-side-button-right"
       >
         &gt;
       </button>
-
-      <div className="carousel-indicators">
-        {slides.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => goToImage(index)}
-            className={`carousel-indicator ${
-              index === currentIndex ? "active" : ""
-            }`}
-          />
-        ))}
-      </div>
     </div>
   );
 };
